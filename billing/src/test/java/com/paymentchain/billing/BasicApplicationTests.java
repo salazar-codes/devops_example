a43f16965cf87eee35cb1e23ead62201575b121b
Jimmy Salazar <jimmyr.salazar@gmail.com>
2024-06-09 21:27:50 -0500

    simulate add test code

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -13,7 +13,7 @@ public class BasicApplicationTests {
  @Test
  public void contextLoads() {
  	    String meessage= "default message cambio test devops 12";
- 	    Assert.assertNotNull(meessage);
+ 	    Assert.assertNotNull(meessage); 
  }
  
 }
